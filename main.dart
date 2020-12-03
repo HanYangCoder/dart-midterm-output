c193719dd487ef80495e4bb6cb451dcb585a51d6
HansoYang647 <hanschristian.herrera@wvsu.edu.ph>
2020-12-03 17:45:40 +0800

    Added a feature that will set score limit (3, 5, 11) and enclosed in a loop

--- main.dart
+++ main.dart
@@ -1,14 +1,46 @@
 import 'dart:io';
+import 'dart:math';
+
+Random random = new Random();
+
+var userScore = 0;
+var compScore = 0;
+var scoreLimitChoices = [3, 5, 11];
+dynamic scoreLimit;
 
 void main(){
   var userChoice;
   var compChoices = ["papel", "gunting", "bato"];
-  var userScore = 0;
-  var pcScore = 0;
   var validSelection = false;
+  int compGuess;
+  bool limitPass = false;
 
   print("Hello there! Welcome to the popular game \"Papel, Gunting, Bato!\"");
 
+  do{
+    print("Please select a score limit (3, 5, 11)");
+    scoreLimit = stdin.readLineSync();
+    scoreLimit = int.parse(scoreLimit);
+
+    print("$scoreLimit");
+
+
+    for(int i=0; i<scoreLimitChoices.length; i++)
+    {
+      if(scoreLimit == scoreLimitChoices[i])
+      {
+        limitPass = true;
+        break;
+      }
+    }
+
+    if(!limitPass)
+    {
+      print("Score limit not valid!");
+    }
+
+  }while(!limitPass);
+    
   do{
     
     print("Please select the weapon of you choice: papel, gunting, or bato");
@@ -18,7 +50,9 @@ void main(){
 
     if((userChoice.compareTo(compChoices[0]) == 0) || (userChoice.compareTo(compChoices[1]) == 0) || (userChoice.compareTo(compChoices[2]) == 0))
     {
-      print("Good choice.");
+      compGuess = random.nextInt(2);
+
+      print('$compGuess');
       validSelection = true;
     }
 
@@ -30,4 +64,8 @@ void main(){
   }while(validSelection == false);
   
   
+}
+
+void compareChoices(){
+
 }
