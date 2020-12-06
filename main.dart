180666bf10f62caaa74d539fdc5dbc5a2dc4404e
HansoYang647 <hanschristian.herrera@wvsu.edu.ph>
2020-12-06 17:46:28 +0800

    Added a play again question and it's working

--- main.dart
+++ main.dart
@@ -7,6 +7,7 @@ var userScore = 0;
 var compScore = 0;
 var scoreLimitChoices = [3, 5, 11];
 dynamic scoreLimit;
+var playAgain = "n";
 
 void main(){
   var userChoice;
@@ -16,74 +17,85 @@ void main(){
   bool limitPass = false;
   bool reachedScoreLimit = false;
 
-  print("Hello there! Welcome to the popular game \"Papel, Gunting, Bato!\"");
-
   do{
-    print("Please select a score limit (3, 5, 11)");
-    scoreLimit = stdin.readLineSync();
-    scoreLimit = int.parse(scoreLimit);
+    print("Hello there! Welcome to the popular game \"Papel, Gunting, Bato!\"");
 
-    print("$scoreLimit");
+    do{
+      print("Please select a score limit (3, 5, 11)");
+      scoreLimit = stdin.readLineSync();
+      scoreLimit = int.parse(scoreLimit);
+
+      print("$scoreLimit");
 
 
-    for(int i=0; i<scoreLimitChoices.length; i++)
-    {
-      if(scoreLimit == scoreLimitChoices[i])
+      for(int i=0; i<scoreLimitChoices.length; i++)
       {
-        limitPass = true;
-        break;
+        if(scoreLimit == scoreLimitChoices[i])
+        {
+          limitPass = true;
+          break;
+        }
+      }
+
+      if(!limitPass)
+      {
+        print("Score limit not valid!");
+      }
+
+    }while(!limitPass);
+      
+    while(!reachedScoreLimit){
+      do{
+      
+      print("Please select the weapon of you choice: papel, gunting, or bato");
+      userChoice = stdin.readLineSync();
+
+      userChoice = userChoice?.toLowerCase();
+
+      if((userChoice.compareTo(compChoices[0]) == 0) || (userChoice.compareTo(compChoices[1]) == 0) || (userChoice.compareTo(compChoices[2]) == 0))
+      {
+        compGuess = random.nextInt(2);
+
+        compareChoices(userChoice, compChoices[compGuess]);
+        validSelection = true;
+      }
+
+      else{
+        print("Invalid choice.");
+        validSelection = false;
+      }
+
+      }while(validSelection == false);
+
+      if(userScore == scoreLimit)
+      {
+        reachedScoreLimit = true;
+      }
+      else if(compScore == scoreLimit)
+      {
+        reachedScoreLimit = true;
       }
     }
 
-    if(!limitPass)
+    if(userScore > compScore)
     {
-      print("Score limit not valid!");
-    }
-
-  }while(!limitPass);
-    
-  while(!reachedScoreLimit){
-    do{
-    
-    print("Please select the weapon of you choice: papel, gunting, or bato");
-    userChoice = stdin.readLineSync();
-
-    userChoice = userChoice?.toLowerCase();
-
-    if((userChoice.compareTo(compChoices[0]) == 0) || (userChoice.compareTo(compChoices[1]) == 0) || (userChoice.compareTo(compChoices[2]) == 0))
-    {
-      compGuess = random.nextInt(2);
-
-      compareChoices(userChoice, compChoices[compGuess]);
-      validSelection = true;
+      print("You win the game! Good game, well played.");
     }
 
     else{
-      print("Invalid choice.");
-      validSelection = false;
+      print("I win the game! I'm better than you!");
     }
 
-    }while(validSelection == false);
+    print("Wanna play again? (y/n): ");
+    playAgain = stdin.readLineSync();
 
-    if(userScore == scoreLimit)
-    {
-      reachedScoreLimit = true;
-    }
-    else if(compScore == scoreLimit)
-    {
-      reachedScoreLimit = true;
-    }
-  }
-
-  if(userScore > compScore)
-  {
-    print("You win the game! Good game, well played.");
-  }
-
-  else{
-    print("I win the game! I'm better than you!");
-  }
-
+    userScore = 0;
+    compScore = 0;
+    limitPass =  false;
+    reachedScoreLimit = false;
+    validSelection = false;
+    
+  }while (playAgain == "y");
 }
 
 void compareChoices(String userChoice, String compChoice){
